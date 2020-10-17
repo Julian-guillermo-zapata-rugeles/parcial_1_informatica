8bb9bfffb975d4a2c9f094d56103fc1fe0208b03
Julian zapata rugeles <juliang.@@>
2020-10-17 16:56:20 -0500

    uso de parabola

--- sistema-defensa/main.cpp
+++ sistema-defensa/main.cpp
@@ -13,6 +13,7 @@ unsigned long long int DISTANCIA_BASES = 0 ;
 
 void movimiento_bala_enemiga( unsigned short int , unsigned short int  ,  unsigned long long int );
 /* @movimiento_bala_enemiga
+ * se implementa la ecuación trayectoria de parabola
         esta función toma 3 argumentos dos unsigned short int
             1) la velocidad 0 del disparo bala enemiga
             2) el angulo con el que fue lanzado la bala enemiga
@@ -30,7 +31,8 @@ int main()
     return 0;
 }
 
-void movimiento_bala_enemiga(unsigned short int velocidad_zero , unsigned short int angulo ,  unsigned long long int DISTANCIA){
-    unsigned short int tmp_position = velocidad_zero*cos(angulo);
-    cout << tmp_position <<" DEBUG position tmp_position " << endl;
+void movimiento_bala_enemiga(unsigned short int velocidad_zero , unsigned short int angulo , unsigned short int time_seconds ){
+    signed long int y_position =
+    cout << "" <<" DEBUG position tmp_position " << endl;
+
 }
