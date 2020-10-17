9d38c993fdf55e1b98becb1c7fb94adf7349aa1a
Julian zapata rugeles <juliang.@@>
2020-10-17 15:52:40 -0500

    Implementación QT -- inicio

---
+++ sistema-defensa/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+unsigned short  int ALTURA_ARMA_ENEMIGA = 0 ;
+unsigned short  int ALTURA_BASE_DEFESA = 0 ;
+unsigned long long int DISTANCIA_ENTRE_BASE = 0 ;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
